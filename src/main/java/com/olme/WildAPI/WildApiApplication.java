20b7803e413e89c183b896634c3a17a1a721d55e
Olme-xD <olmem.molina02@gmail.com>
2025-10-04 13:35:01 -0400

    Added springboot files

---
+++ src/main/java/com/olme/WildAPI/WildApiApplication.java
@@ -0,0 +1,13 @@
+package com.olme.WildAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olme/WildAPI/WildApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olme.WildAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
